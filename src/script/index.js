b5a9fa35650a0f408820bf9d8dc056e360303c16
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-03-30 20:56:05 -0700

    succesfullly pulled from the api and logged it to the console

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,5 @@
 import '../styles/styles.css';
 console.log('Hello World');
+let weather = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=W2FDGJ2MLL2LU727Y6D8PWFW5', {mode: 'cors'})
+weather = await weather.json()
+console.log(weather);
